b3ab335582ec15642aa48338c866d27acb0969bb
renatogcruz <renatogcruz@hotmail.com>
2023-08-14 21:55:35 -0300

    add files book

---
+++ chapter03/README.md
@@ -0,0 +1,12 @@
+
+
+## Coral
+
+1. Go through the get started guide https://coral.ai/docs/accelerator/get-started
+1. Find the iNat models at https://coral.ai/models/
+1. Use the common-fly.jpg in this repo to run the classification
+
+
+Note: Common Fly image via Pixabay License (Free for commercial use and no attribution required):
+  https://pixabay.com/photos/macro-fly-nature-insect-bug-green-1802322/
+
---
+++ chapter06/multiple-loggers/http-app.py
@@ -0,0 +1,22 @@
+import requests
+import logging
+
+logging.basicConfig()
+root_logger = logging.getLogger()
+
+# Sets logging level for every single app, the "parent" logger
+root_logger.setLevel(logging.DEBUG)
+
+# new logger for this script
+logger = logging.getLogger('http-app')
+
+
+logger.info("About to send a request to example.com")
+requests.get('http://example.com')
+
+# fine tune the urllib logger:
+urllib_logger = logging.getLogger('urllib3')
+urllib_logger.setLevel(logging.ERROR)
+#
+logger.info("About to send another request to example.com")
+requests.get('http://example.com')
